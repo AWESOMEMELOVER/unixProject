a017e517ab3428267400f31cda17e7f77a03d863
AWESOMEMELOVER <nikitakyrpenko@gmail.com>
2017-06-21 20:47:48 +0300

    Minor Fx

--- js/index.js
+++ js/index.js
@@ -13,20 +13,20 @@ $(function() {
 
 			});
 
-			var roundSlider = $(".roundSlider").CircularSlider({
-				min:0,
+			var roundSlider1 = $(".roundSlider1").CircularSlider({
+				min:-100,
 				max:100,
 				radius:35
 			});
 
 			var roundSlider2 = $(".roundSlider2").CircularSlider({
-				min:0,
+				min:-100,
 				max:100,
 				radius:35
 			});		
 			
 			var roundSlider3 = $(".roundSlider3").CircularSlider({
-				min:0,
+				min:-100,
 				max:100,
 				radius:35
 			});		
