c952f8cf7bda3404e7fd613a21cd6b46baf28f92
t.babich@lumination.com.ua <t.babich@lumination.com.ua>
2017-06-25 22:16:58 +0300

    layout fix

--- js/index.js
+++ js/index.js
@@ -1,65 +1,64 @@
 $(function() {
-			 var handle = $( "#custom-handle" );
-			//Store frequently elements in variables
-			var cross  = $('#cross').slider({
-				min:0,
-				max:100,
-				create: function() {
-					handle.text( $( this ).slider( "value" ) );
-      				},
-				slide: function( event, ui ) {
-					handle.text( ui.value );
-					}
-			});
-    
-            var cross  = $('#cross2').slider({
-				min:0,
-				max:100
-			});
+var handle = $( "#custom-handle" );
+//Store frequently elements in variables
 
-			var slider2  = $('#slider2').slider({
-				min:0,
-				max:100
-			});
+var cross  = $('#cross').slider({
+	orientation: "vertical",
+	min:0,
+	max:100,
+	create: function() {
+		handle.text( $( this ).slider( "value" ) );
+	},
+	slide: function( event, ui ) {
+		handle.text( ui.value );
+	}
+});
 
-			var roundSlider1 = $(".roundSlider1").CircularSlider({
-				min:-100,
-				max:100,
-				radius:35
-			});
+var cross  = $('#cross2').slider({
+	orientation: "vertical",
+	min:0,
+	max:100
+});
 
-			var roundSlider2 = $(".roundSlider2").CircularSlider({
-				min:-100,
-				max:100,
-				radius:35
-			});		
-			
-			var roundSlider3 = $(".roundSlider3").CircularSlider({
-				min:-100,
-				max:100,
-				radius:35
-			});		
-    
-            var roundSlider4 = $(".roundSlider4").CircularSlider({
-				min:-100,
-				max:100,
-				radius:35
-			});
+var slider2  = $('#slider2').slider({
+	min:0,
+	max:100
+});
 
-			var roundSlider5 = $(".roundSlider5").CircularSlider({
-				min:-100,
-				max:100,
-				radius:35
-			});		
-			
-			var roundSlider6 = $(".roundSlider6").CircularSlider({
-				min:-100,
-				max:100,
-				radius:35
-			});		
-            var myDropzone = new Dropzone("div#my-awesome-dropzone", { url: "/file/post"});       
-    
-        
-    
+var roundSlider1 = $(".roundSlider1").CircularSlider({
+	min:-100,
+	max:100,
+	radius:35
+});
 
-		});
+var roundSlider2 = $(".roundSlider2").CircularSlider({
+	min:-100,
+	max:100,
+	radius:35
+});		
+
+var roundSlider3 = $(".roundSlider3").CircularSlider({
+	min:-100,
+	max:100,
+	radius:35
+});		
+
+var roundSlider4 = $(".roundSlider4").CircularSlider({
+	min:-100,
+	max:100,
+	radius:35
+});
+
+var roundSlider5 = $(".roundSlider5").CircularSlider({
+	min:-100,
+	max:100,
+	radius:35
+});		
+
+var roundSlider6 = $(".roundSlider6").CircularSlider({
+	min:-100,
+	max:100,
+	radius:35
+});		
+// var myDropzone = new Dropzone("div#my-awesome-dropzone", { url: "/file/post"});
+});
