84d52aa9ad1da1cbded05d585928dd7e3b758322
AWESOMEMELOVER <nikitakyrpenko@gmail.com>
2017-06-26 22:59:39 +0300

    minor fixes

--- js/draganddrop.js
+++
@@ -1,8 +0,0 @@
- thumbnail: function("#my-awesome-dropzone") {
-            var thumbnail = $('.dropzone .dz-preview.dz-file-preview');
-            thumbnail.css('background', 'url('+dataUrl+')');
-            var $fotoramaDiv = $('.fotorama').fotorama();
-            var fotorama = $fotoramaDiv.data('fotorama');
-            fotorama.push({img: dataUrl, thumb:dataUrl});   
-                }
-
